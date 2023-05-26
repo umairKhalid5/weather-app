a9a63dda4b1f61f50aad6ae212242b8781ba1b3e
Umair <rajaumairkhalid555@gmail.com>
2023-05-26 17:25:25 +0500

    fixed graph bug

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -85,9 +85,9 @@ const Home = () => {
       style={{
         backgroundImage: `url(/${bgImage}.jpg)`,
       }}
-      initial={{ opacity: 0, scale: 0.5 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.5 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
       {/* //?Left */}
